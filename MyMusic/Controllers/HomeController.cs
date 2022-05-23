a438a5aa40ee0023c4a556b60c129e54a31ec391
MPilot93 <marco.plt.93@gmail.com>
2022-05-23 14:42:32 +0200

    View Aggiunte

--- MyMusic/Controllers/HomeController.cs
+++ MyMusic/Controllers/HomeController.cs
@@ -10,11 +10,19 @@ namespace MyMusic.Controllers
     {
         private readonly ILogger<HomeController> _logger;
         private BraniManager branoDB;
+        private AlbumManager albumDB;
+        private BandManager bandDB;
+        private ArtistiManager artistiDB;
+
 
         public HomeController(ILogger<HomeController> logger)
         {
             _logger = logger;
             branoDB = new BraniManager();
+            artistiDB= new ArtistiManager();
+            albumDB= new AlbumManager();
+            bandDB= new BandManager();
+            
         }
 
         [HttpGet]
@@ -25,9 +33,53 @@ namespace MyMusic.Controllers
             return View(result);
         }
 
-        public IActionResult Privacy()
+
+        [HttpGet]
+        public IActionResult AggiungiBrano()
         {
             return View();
+        }
+        [HttpPost]
+        public IActionResult AggiungiBrano(BranoViewModel brano)
+        {
+            branoDB.AggiungiBrano(brano);
+            return RedirectToAction("Index");
+        }
+
+        [HttpGet]
+        public IActionResult AggiungiAlbum()
+        {
+            return View();
+        }
+        [HttpPost]
+        public IActionResult AggiungiAlbum(AlbumViewModel album)
+        {
+            albumDB.AggiungiAlbum(album);
+            return RedirectToAction("Index");
+        }
+
+        [HttpGet]
+        public IActionResult AggiungiArtista()
+        {
+            return View();
+        }
+        [HttpPost]
+        public IActionResult AggiungiArtista(ArtistiViewModel artista)
+        {
+            artistiDB.AggiungiArtisti(artista);
+            return RedirectToAction("Index");
+        }
+
+        [HttpGet]
+        public IActionResult AggiungiBand()
+        {
+            return View();
+        }
+        [HttpPost]
+        public IActionResult AggiungiBand(BandViewModel band)
+        {
+            bandDB.AggiungiBand(band);
+            return RedirectToAction("Index");
         }
 
         [ResponseCache(Duration = 0, Location = ResponseCacheLocation.None, NoStore = true)]
